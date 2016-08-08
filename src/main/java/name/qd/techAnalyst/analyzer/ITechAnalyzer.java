d00d171ce0336020720813910abbf7ed5a6fe291
shawn <Shawn.Chou@nogle.com>
2016-08-08 15:37:58 +0800

    1. 更多的均價

--- src/main/java/name/qd/techAnalyst/analyzer/ITechAnalyzer.java
+++ src/main/java/name/qd/techAnalyst/analyzer/ITechAnalyzer.java
@@ -7,5 +7,5 @@ import name.qd.techAnalyst.vo.AnalysisResult;
 
 public interface ITechAnalyzer {
 
-	public List<AnalysisResult> analyze(TWSEDataManager dataManager, String sFrom, String sTo, String sProd);
+	public List<AnalysisResult> analyze(TWSEDataManager dataManager, String sFrom, String sTo, String sProdId);
 }
---
+++ src/main/java/name/qd/techAnalyst/analyzer/impl/MovingAvg60Day.java
@@ -0,0 +1,26 @@
+package name.qd.techAnalyst.analyzer.impl;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import name.qd.techAnalyst.analyzer.AnalystUtils;
+import name.qd.techAnalyst.analyzer.ITechAnalyzer;
+import name.qd.techAnalyst.dataSource.TWSEDataManager;
+import name.qd.techAnalyst.vo.AnalysisResult;
+import name.qd.techAnalyst.vo.ProdClosingInfo;
+
+public class MovingAvg60Day implements ITechAnalyzer {
+	@Override
+	public List<AnalysisResult> analyze(TWSEDataManager dataManager, String sFrom, String sTo, String sProdId) {
+		ArrayList<ProdClosingInfo> lst = null;
+		try {
+			lst = dataManager.getProdClosingInfo(sFrom, sTo, sProdId);
+		} catch (ParseException | IOException e) {
+			// TODO
+			e.printStackTrace();
+		}
+		return AnalystUtils.NDaysAvg(lst, 60);
+	}
+}
