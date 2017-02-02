97972c3b31b51c4e70178aff4f1343161ad4bb8d
shawn <Shawn.Chou@nogle.com>
2017-02-02 12:13:52 +0800

    1. modify date

--- src/main/java/name/qd/techAnalyst/TechAnalyst.java
+++ src/main/java/name/qd/techAnalyst/TechAnalyst.java
@@ -28,7 +28,7 @@ public class TechAnalyst {
 		analyzerManager = new TechAnalyzerManager();
 		twseDataManager = new TWSEDataManager("./file/");
 		
-		String sFrom = "20160302";
+		String sFrom = "20160301";
 		String sTo = "20160601";
 		String sProdId = "2453";
 		String sAnalyzer = MovingAvg5Day.class.getSimpleName();
