989f6389c953a5d115a2292991a12290d2cd1073
shawn <Shawn.Chou@nogle.com>
2018-02-14 17:41:48 +0800

    1. verify result

---
+++ src/main/java/name/qd/techAnalyst/vo/VerifyResult.java
@@ -0,0 +1,20 @@
+package name.qd.techAnalyst.vo;
+
+import java.util.Date;
+
+public class VerifyResult {
+	private double winPercent;
+	private Date date;
+	public double getWinPercent() {
+		return winPercent;
+	}
+	public void setWinPercent(double winPercent) {
+		this.winPercent = winPercent;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
--- src/main/java/name/qd/techAnalyst/winPercent/impl/ABIVerify.java
+++ src/main/java/name/qd/techAnalyst/winPercent/impl/ABIVerify.java
@@ -1,7 +1,14 @@
 package name.qd.techAnalyst.winPercent.impl;
 
+import java.util.List;
+
+import name.qd.techAnalyst.vo.AnalysisResult;
+import name.qd.techAnalyst.vo.VerifyResult;
 import name.qd.techAnalyst.winPercent.WinningPercentageCalc;
 
 public class ABIVerify implements WinningPercentageCalc {
-
+	
+	public VerifyResult verify(List<AnalysisResult> lst) {
+		return null;
+	}
 }
