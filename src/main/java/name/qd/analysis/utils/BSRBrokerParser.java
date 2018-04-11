b6a661ca7c6880648401a221bce2a4ec8185b742
shawn <Shawn.Chou@nogle.com>
2018-04-11 13:15:19 +0800

    1. remove captcha jpg

---
+++ src/main/java/name/qd/analysis/utils/BSRBrokerParser.java
@@ -0,0 +1,12 @@
+package name.qd.analysis.utils;
+
+public class BSRBrokerParser {
+	
+	private BSRBrokerParser() {
+		
+	}
+
+	public static void main(String[] args) {
+		new BSRBrokerParser();
+	}
+}
