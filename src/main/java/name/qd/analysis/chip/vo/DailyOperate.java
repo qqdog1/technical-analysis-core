faa4293c332c95a549c569e35e304b2bb6a09a47
qqdog1 <qqdog1@yahoo.com.tw>
2019-05-27 01:56:34 +0800

    1. DailyOperate object extends CoordinateObject

--- src/main/java/name/qd/analysis/chip/vo/DailyOperate.java
+++ src/main/java/name/qd/analysis/chip/vo/DailyOperate.java
@@ -1,8 +1,14 @@
 package name.qd.analysis.chip.vo;
 
+import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
 
-public class DailyOperate {
+import name.qd.fileCache.cache.CoordinateObject;
+import name.qd.fileCache.common.TransInputStream;
+import name.qd.fileCache.common.TransOutputStream;
+
+public class DailyOperate extends CoordinateObject {
 	private Date date;
 	private String brokerName;
 	private String product;
@@ -58,5 +64,41 @@ public class DailyOperate {
 	}
 	public void setPnlRate() {
 		pnlRate = pnl/tradeCost;
+	}
+	@Override
+	public String getXKey() {
+		return product;
+	}
+	@Override
+	public String getYKey() {
+		return brokerName;
+	}
+	@Override
+	public byte[] parseToFileFormat() throws IOException {
+		TransOutputStream tOut = new TransOutputStream();
+		tOut.writeLong(date.getTime());
+		tOut.writeString(product);
+		tOut.writeString(brokerName);
+		tOut.writeDouble(pnl);
+		tOut.writeDouble(tradeCost);
+		tOut.writeLong(openShare);
+		tOut.writeDouble(avgPrice);
+		tOut.writeDouble(pnlRate);
+		return tOut.toByteArray();
+	}
+	@Override
+	public void toValueObject(byte[] data) throws IOException {
+		TransInputStream tIn = new TransInputStream(data);
+		long timestamp = tIn.getLong();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(timestamp);
+		date = calendar.getTime();
+		product = tIn.getString();
+		brokerName = tIn.getString();
+		pnl = tIn.getDouble();
+		tradeCost = tIn.getDouble();
+		openShare = tIn.getLong();
+		avgPrice = tIn.getDouble();
+		pnlRate = tIn.getDouble();
 	}
 }
